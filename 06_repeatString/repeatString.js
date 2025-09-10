be6e8000edc1a1fa4b34eed3e14b3a8d19fff7bc
Mohammad Hamzah J. Tomawis <hamzatomawis333@gmail.com>
2025-09-10 19:29:09 +0800

    Tomawis 06 test

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, num) {
+    if (num < 0) return "ERROR"
+  
+    let result = ""
+    for (let i = 0; i < num; i++) {
+      result += string
+    }
+  
+    return result
+  }
+  
+  module.exports = repeatString
+  
 
 // Do not edit below this line
 module.exports = repeatString;
