cc064abcc816ae538e8cdc31ba8103cea2c3ad89
Mohammad Hamzah J. Tomawis <hamzatomawis333@gmail.com>
2025-09-10 21:36:41 +0800

    Tomawis 14 test

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
-
-};
-
-// Do not edit below this line
-module.exports = fibonacci;
+const fibonacci = function(n) {
+    // convert string inputs to number
+    n = Number(n);
+  
+    if (n < 0) return "OOPS";
+    if (n === 0) return 0;
+    if (n === 1 || n === 2) return 1;
+  
+    let a = 1;
+    let b = 1;
+  
+    for (let i = 3; i <= n; i++) {
+      let temp = a + b;
+      a = b;
+      b = temp;
+    }
+  
+    return b;
+  };
+  
+  // Do not edit below this line
+  module.exports = fibonacci;
+  
