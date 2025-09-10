2be75bcd590b1fa02cd64b9df2dbce757b0c6596
Mohammad Hamzah J. Tomawis <hamzatomawis333@gmail.com>
2025-09-10 21:11:26 +0800

    Tomawis 09 test

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
-};
+const sumAll = function(a, b) {
+    // check invalid inputs
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+      return "ERROR";
+    }
+  
+    let start = Math.min(a, b);
+    let end = Math.max(a, b);
+    let sum = 0;
+  
+    for (let i = start; i <= end; i++) {
+      sum += i;
+    }
+  
+    return sum;
+  };
+  
 
 // Do not edit below this line
 module.exports = sumAll;
