f0d71a6445677ec84401aa2e2fa6d6c683f7091e
Mohammad Hamzah J. Tomawis <hamzatomawis333@gmail.com>
2025-09-10 21:32:05 +0800

    Tomawis 13 test

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,11 +1,9 @@
 const palindromes = function (string) {
-    // normalize: lowercase + remove non-alphanumeric
+    
     let cleaned = string.toLowerCase().replace(/[^a-z0-9]/g, '');
     
-    // reverse
     let reversed = cleaned.split('').reverse().join('');
     
-    // compare
     return cleaned === reversed;
   };
   
