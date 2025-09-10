04471dde38ec3c82520ddbb7e08deb937f412482
Mohammad Hamzah J. Tomawis <hamzatomawis333@gmail.com>
2025-09-10 21:54:36 +0800

    Tomawis 16 test

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
-
-};
-
-// Do not edit below this line
-module.exports = findTheOldest;
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+  
+    return people.reduce((oldest, person) => {
+      const personAge = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+      const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+  
+      return personAge > oldestAge ? person : oldest;
+    });
+  };
+  
+  // Do not edit below this line
+  module.exports = findTheOldest;
+  
