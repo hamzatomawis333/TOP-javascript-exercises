5c46d2620fb2bb95473fe6638cf386c4b79f64cf
Mohammad Hamzah J. Tomawis <hamzatomawis333@gmail.com>
2025-09-10 20:50:34 +0800

    Tomawis 07 test

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
-};
+const reverseString = function(a) {
+    let reversed = '';
+    // start from the last character and move backwards
+    for (let i = a.length - 1; i >= 0; i--) {
+      reversed += a[i];  // add each character to the new string
+    }
+    return reversed;
+  };
 
 // Do not edit below this line
 module.exports = reverseString;
